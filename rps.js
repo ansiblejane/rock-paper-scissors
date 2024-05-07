1b190b0474f6cfae78c400cdded4d651d1a3ff86
ansiblejane <33788618+ansiblejane@users.noreply.github.com>
2024-05-06 19:33:35 -0700

    Added a reset button

--- rps.js
+++ rps.js
@@ -11,106 +11,122 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection=="rock") {
         if (computerSelection=="rock") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". It's a tie!"), [1,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". This round is a tie!"), [1,1]]
         } else if (computerSelection=="paper") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose!"), [0,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose this round!"), [0,1]]
         } else if (computerSelection=="scissors") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win!"), [1,0]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win this round!"), [1,0]]
         } else {
             return "Huh..."
         }
     }
     if (playerSelection=="paper") {
         if (computerSelection=="paper") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". It's a tie!"), [1,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". This round is a tie!"), [1,1]]
         } else if (computerSelection=="scissors") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose!"), [0,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose this round!"), [0,1]]
         } else if (computerSelection=="rock") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win!"), [1,0]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win this round!"), [1,0]]
         } else {
             return "Huh..."
         }
     }
     if (playerSelection=="scissors") {
         if (computerSelection=="scissors") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". It's a tie!"), [1,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". This round is a tie!"), [1,1]]
         } else if (computerSelection=="rock") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose!"), [0,1]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You lose this round!"), [0,1]]
         } else if (computerSelection=="paper") {
-            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win!"), [1,0]]
+            return [("You played " + playerSelection + ". The computer played " + computerSelection + ". You win this round!"), [1,0]]
         } else {
             return "Huh..."
         }
     }
 }
 
-
-/* 
-function game(rounds) {
-    let score = [0,0]
-
-    for(let i = 0; i < rounds; i++) {
-        let roundResults = playRound(prompt("Enter your shape: "), getComputerChoice())
-        let roundMessage = roundResults[0]
-        let roundScore = roundResults[1]
-        score = [(score[0] + roundScore[0]), (score[1] + roundScore[1])]
-        console.log(roundMessage + " The score is " + score[0] + "-" + score[1] + ".")
-    }
-
-    if (score[0] > score[1]) {
-        console.log("The final score is " + score[0] + "-" + score[1] + ". You win!")
-    } else if (score[1] > score[0]) {
-        console.log("The final score is " + score[0] + "-" + score[1] + ". You lose!")
-    } else {
-        console.log("The final score is " + score[0] + "-" + score[1] + ". It's a tie!")
-    }
-}
-*/
-
 let totalScore = [0,0];
 
 const body = document.querySelector("body");
-
+const roundMessage = document.createElement("div");
 const scoreboard = document.createElement("div");
+const gameMessage = document.createElement("div");
 
+body.appendChild(roundMessage);
 body.appendChild(scoreboard);
+body.appendChild(gameMessage);
 
-function declareWinner(totalScore) {
-    if (totalScore[0]>=5){
-        scoreboard.textContent += "The player wins!";
-    } else if (totalScore[1]>=5){
-        scoreboard.textContent += "The computer wins!";
+scoreboard.textContent = "The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
+gameMessage.textContent = "The first to 5 wins!";
+
+/* FUNCTIONS */
+
+function updateScoreboard(totalScore){
+    scoreboard.textContent = "The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
+}
+
+function resetGame(){
+    totalScore = [0,0];
+}
+
+function updateGameMessage(totalScore) {
+    if (totalScore[0] == 5 && totalScore[1] == 5){
+        gameMessage.textContent = "It's a tie!";
+    } else if (totalScore[0] == 5){
+        gameMessage.textContent = "The player wins!";
+    } else if (totalScore[1] == 5){
+        gameMessage.textContent = "The computer wins!";
     } else {
-        scoreboard.textContent += "The first to 5 wins!";
+        gameMessage.textContent = "The first to 5 wins!";
     }
 }
 
+/* BUTTONS */
+
+const btnReset = document.querySelector('#btn-reset');
+btnReset.addEventListener('click', () => {
+    resetGame();
+    roundMessage.textContent = "";
+    updateScoreboard(totalScore);
+});
+
 const btnRock = document.querySelector('#btn-rock');
 btnRock.addEventListener('click', () => {
-    let results = playRound("rock", getComputerChoice());
-    let message = results[0]; 
-    let score = results[1];
-    totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
-    scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
-    declareWinner(totalScore);
+    if (totalScore[0] >= 5 || totalScore[1] >= 5){
+        alert("Click Reset to keep playing!");
+    } else {
+        let results = playRound("rock", getComputerChoice());
+        totalScore = [totalScore[0] + results[1][0], totalScore[1] + results[1][1]];
+    
+        roundMessage.textContent = results[0];
+        updateScoreboard(totalScore);
+        updateGameMessage(totalScore); 
+    }
 });
 
 const btnPaper = document.querySelector('#btn-paper');
 btnPaper.addEventListener('click', () => {
-    let results = playRound("paper", getComputerChoice());
-    let message = results[0]; 
-    let score = results[1];
-    totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
-    scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
-    declareWinner(totalScore);
+    if (totalScore[0] >= 5 || totalScore[1] >= 5){
+        alert("Click Reset to keep playing!");
+    } else {
+        let results = playRound("paper", getComputerChoice());
+        totalScore = [totalScore[0] + results[1][0], totalScore[1] + results[1][1]];
+    
+        roundMessage.textContent = results[0];
+        updateScoreboard(totalScore);
+        updateGameMessage(totalScore); 
+    }
 });
 
 const btnScissors = document.querySelector('#btn-scissors');
 btnScissors.addEventListener('click', () => {
-    let results = playRound("scissors", getComputerChoice());
-    let message = results[0]; 
-    let score = results[1];
-    totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
-    scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
-    declareWinner(totalScore);
+    if (totalScore[0] >= 5 || totalScore[1] >= 5){
+        alert("Click Reset to keep playing!");
+    } else {
+        let results = playRound("scissors", getComputerChoice());
+        totalScore = [totalScore[0] + results[1][0], totalScore[1] + results[1][1]];
+    
+        roundMessage.textContent = results[0];
+        updateScoreboard(totalScore);
+        updateGameMessage(totalScore); 
+    }
 });
