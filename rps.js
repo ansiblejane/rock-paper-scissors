74457bb5f8f8c2c4d2069d7701a93420e8e6e658
ansiblejane <33788618+ansiblejane@users.noreply.github.com>
2024-05-06 18:30:08 -0700

    Duct tape version. It works?

--- rps.js
+++ rps.js
@@ -75,6 +75,16 @@ const scoreboard = document.createElement("div");
 
 body.appendChild(scoreboard);
 
+function declareWinner(totalScore) {
+    if (totalScore[0]>=5){
+        scoreboard.textContent += "The player wins!";
+    } else if (totalScore[1]>=5){
+        scoreboard.textContent += "The computer wins!";
+    } else {
+        scoreboard.textContent += "The first to 5 wins!";
+    }
+}
+
 const btnRock = document.querySelector('#btn-rock');
 btnRock.addEventListener('click', () => {
     let results = playRound("rock", getComputerChoice());
@@ -82,17 +92,25 @@ btnRock.addEventListener('click', () => {
     let score = results[1];
     totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
     scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
+    declareWinner(totalScore);
 });
 
 const btnPaper = document.querySelector('#btn-paper');
 btnPaper.addEventListener('click', () => {
-    console.log(playRound("paper",getComputerChoice()));
+    let results = playRound("paper", getComputerChoice());
+    let message = results[0]; 
+    let score = results[1];
+    totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
+    scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
+    declareWinner(totalScore);
 });
 
 const btnScissors = document.querySelector('#btn-scissors');
 btnScissors.addEventListener('click', () => {
-    console.log(playRound("scissors",getComputerChoice()));
+    let results = playRound("scissors", getComputerChoice());
+    let message = results[0]; 
+    let score = results[1];
+    totalScore = [totalScore[0] + score[0], totalScore[1] + score[1]];
+    scoreboard.textContent = message + " The player score is " + totalScore[0] + " and the computer score is " + totalScore[1] + ".";
+    declareWinner(totalScore);
 });
-
-
-
